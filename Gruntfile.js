cab4db505472f21281ce370e00ab4f1b5ac57454
aisinvon <aisinvon@gmail.com>
2013-07-26 21:18:41 +0800

    Backup my Grunt setting files
    Backup my Grunt setting files

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+
+    // 构建任务配置
+    grunt.initConfig({
+        //读取package.json的内容，形成json数据
+        pkg: grunt.file.readJSON('package.json'),
+
+        //js min
+        uglify: {
+            //文件头部输出信息
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> by fwx*/\n'
+            },
+            //具体任务配置
+            build: {
+                files: {
+                    'js/mobile.min.js': 'js/mobile.js',
+                    'js/v2-mobile.min.js': 'js/v2-mobile.js',
+                    'js/index.min.js': 'js/index.js'
+                }
+                //源文件
+                //src: 'index.js',
+                //src: 'v2-mobile.js',
+                //目标文件
+                //dest: 'index.min.js'
+                //dest: 'v2-mobile.min.js'
+            }
+        },
+
+
+        //css min
+        cssmin: {
+            //with_banner: {
+            //文件头部输出信息
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> by fwx*/\n'
+            },
+            //具体任务配置
+            combine: {
+                files: {
+                    //style下顶级目录css压缩
+                    //'style/mobile.min.css': ['style/headFoot.css','style/common.css','style/mobile.css'],
+                    //'style/promo.min.css': ['style/headFoot.css','style/common.css','style/promoNew.css'],
+
+                    //style下v2目录css压缩
+                    'style/v2/index.min.css': ['style/v2/01reset.css', 'style/v2/02common.css', 'style/v2/03headFoot.css', 'style/v2/04index.css'],
+                    'style/v2/mobile.min.css': ['style/v2/01reset.css', 'style/v2/02common.css', 'style/v2/03headFoot.css', 'style/v2/05mobile.css', 'style/v2/05mobile-02.css'],
+                    'style/v2/promoNew.min.css': ['style/v2/01reset.css', 'style/v2/02common.css', 'style/v2/03headFoot.css', 'style/v2/06promoNew.css']
+                }
+            }
+
+            //}
+        }
+
+
+
+    });
+
+    // 加载指定插件任务
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // 默认执行的任务
+    grunt.registerTask('default', ['uglify', 'cssmin']);
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+###use grunt-contrib-uglify to compress JS
+
+###use grunt-contrib-cssmin to merge and compress CSS
